949d338403b267bf888bd7111498988be13a89d6
pyrex41 <reub.brooks@gmail.com>
2025-04-11 16:53:20 -0500

    fix logo size-mobile

--- frontend/src/Utils/QuoteHeader.elm
+++ frontend/src/Utils/QuoteHeader.elm
@@ -9,7 +9,7 @@ viewHeader maybeImage maybeName =
     div [ class "flex justify-center items-center mt-4 mb-6" ]
         [ case ( maybeImage, maybeName ) of
             ( Just logo, _ ) ->
-                img [ src logo, alt "Organization Logo", class "h-16 max-w-[300px] object-contain px-4" ] []
+                img [ src logo, alt "Organization Logo", class "h-16 max-w-[240px] md:max-w-[300px] object-contain px-4" ] []
 
             ( _, Just name ) ->
                 div [ class "text-2xl font-bold text-[#101828] leading-[1.2]" ] [ text name ]
