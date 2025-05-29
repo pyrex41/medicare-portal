7629341bb2b3fd669026cc4e560a1bb9962d16d1
pyrex41 <reub.brooks@gmail.com>
2025-05-29 01:52:09 -0500

    tweak background

--- frontend/src/ContactUs.elm
+++ frontend/src/ContactUs.elm
@@ -129,7 +129,7 @@ view model =
 
 viewContent : Model -> Html Msg
 viewContent model =
-    div [ class "min-h-screen bg-gray-50" ]
+    div [ class "min-h-screen bg-white" ]
         [ div [ class "max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8" ]
             [ div [ class "text-center mb-12" ]
                 [ h1 [ class "text-4xl font-bold text-gray-900 mb-4" ]
